8f4142f2a15ffa4517db60851183e9af0ff71988
Rodrigo Sanchez <rosv58@gmail.com>
2025-04-03 13:35:17 -0600

    Making it dynamic?

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@
 import ProductList from '@/components/shared/product/product-list';
 import {getLatestProducts} from '@/lib/actions/product.actions';
 
-
+export const dynamic = "force-dynamic";
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
