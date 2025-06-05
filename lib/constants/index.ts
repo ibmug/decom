bee71e7c375d4bacccd82732e7ba3d678630a9be
Rodrigo Sanchez <rosv58@gmail.com>
2025-06-05 06:21:37 -0600

    Adding stuff to ci

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,7 +1,7 @@
 export const APP_NAME = process.env.PUBLIC_APP_NAME || 'Tiendita';
 export const APP_DESCRIPTION =  process.env.PUBLIC_DESCRIPTION || 'Una tiendita'
 export const SERVER_URL = process.env.PUBLIC_SERVER_URL || 'http://localhost:3000'
-export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
+export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 9;
 
 
 export const PAYMENT_METHODS = process.env.PAYMENT_METHODS ? process.env.PAYMENT_METHODS.split(', '): ['PayPal', 'Stripe','CashOnPickup'];
